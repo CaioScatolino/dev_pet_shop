5975855536da0d03e76a8e3871a6d7312a99f67d
Caio <caio.scatolino@outlook.com>
2026-01-07 18:56:37 -0300

    feat: set up initial Express server using ES modules, dotenv, and mustache

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import mustache from "mustache-express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Configurando o dotenv
+dotenv.config();
+
+const server = express();
+
+server.set("view engine", "mustache");
+server.set("views", path.join(__dirname, "views"));
+server.engine("mustache", mustache());
+
+server.use(express.static(path.join(__dirname, "../public")));
+
+// Rotas
+
+server.listen(process.env.PORT, () => {
+    console.log("Server is running on port " + process.env.PORT);
+})
